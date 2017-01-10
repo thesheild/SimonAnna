0d5bcad06b73129db8051023178308a7d7ddf644
Student6 <Student6@10.8.33.161>
2017-01-10 13:17:57 -0500

    dfsgsfg

--- SAnna/src/partnerCodeInHerePlease/Button.java
+++ SAnna/src/partnerCodeInHerePlease/Button.java
@@ -1,47 +1,31 @@
-package partnerCodeInHerePlease;
+package PartnerCodeInHerePlease;
 
 import java.awt.image.BufferedImage;
 
 import SimonAnna.ButtonInterfaceAnna;
 
-public class Button  implements ButtonInterfaceAnna{
+public class Button implements ButtonInterfaceAnna {
 
 	public Button() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void update() {
-		
-		
-	}//
 	@Override
-	public void act() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean isHovered(int arg0, int arg1) {
+	public boolean isHovered(int x, int y) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public int getHeight() {
+	public void act() {
 		// TODO Auto-generated method stub
-		return 0;
+
 	}
 
 	@Override
 	public BufferedImage getImage() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
@@ -57,11 +41,27 @@ public class Button  implements ButtonInterfaceAnna{
 	}
 
 	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
 	public boolean isAnimated() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
 
+	}
 
 }
